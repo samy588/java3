55319786f6e4ba6e88ebb6b4e154938094b8dd33
samy588 <71956118+samy588@users.noreply.github.com>
2020-10-18 08:30:10 +0530

    Add files via upload

---
+++ dd.java
@@ -0,0 +1,24 @@
+package dd;
+
+public class dd {
+	public static void main(String args[])
+	{
+	int n,i;
+	n=f(8);
+	System.out.println(n);
+	}
+	public   static int f(int x)
+	{
+		if(x==0)
+		{
+			return x;
+			
+		}
+		else 
+			return x+(x-1);
+		
+		}
+		
+	}
+
+
